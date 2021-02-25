eb8d019f02b197db3aed9bb6ef3bd4b1c0a66fe6
Rob Smith <rob@leadspot.co.uk>
2021-02-25 14:01:29 +0000

    Add project files.

---
+++ Pages/Apply.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LivelyLoans.Pages
+{
+    public class ApplyModel : PageModel
+    {
+        private readonly ILogger<ApplyModel> _logger;
+
+        public ApplyModel(ILogger<ApplyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Complaints.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LivelyLoans.Pages
+{
+    public class ComplaintsModel : PageModel
+    {
+        private readonly ILogger<ComplaintsModel> _logger;
+
+        public ComplaintsModel(ILogger<ComplaintsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
